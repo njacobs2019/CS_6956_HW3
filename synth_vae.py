6212f5c245db0ed83c32788cefdd4cfcebab222e
Nicholas Jacobs <59352054+njacobs2019@users.noreply.github.com>
2025-03-28 23:02:42 -0600

    Got synth vae working

--- synth_vae.py
+++ synth_vae.py
@@ -87,6 +87,7 @@ def main(args):
         checkpoint_name=experiment_name,
         log_every=5,  # comet log every 10 batches
         save_reconstructions_flag=False,
+        scheduler_steps=1_000_000,
     )
 
     return model
